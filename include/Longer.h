dd44c1881e7b52d3fcb4bc1bbb50694032a3c3f6
Nircek <marcinek0302@gmail.com>
2017-03-28 17:00:36 +0200

    init

---
+++ include/Longer.h
@@ -0,0 +1,17 @@
+#ifndef LONGER_H
+#define LONGER_H
+
+
+class Longer
+{
+    public:
+        Longer();
+        virtual ~Longer();
+        Longer(const Longer& other);
+
+    protected:
+
+    private:
+};
+
+#endif // LONGER_H
---
+++ src/Longer.cpp
@@ -0,0 +1,16 @@
+#include "Longer.h"
+
+Longer::Longer()
+{
+    //ctor
+}
+
+Longer::~Longer()
+{
+    //dtor
+}
+
+Longer::Longer(const Longer& other)
+{
+    //copy ctor
+}
